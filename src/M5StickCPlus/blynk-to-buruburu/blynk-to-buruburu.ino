e588943a609a2ecd1dd50fb268da50363b8e89de
Ryos Suzuki <ryos@sinby.com>
2022-02-21 00:47:10 +0900

    画像追加

--- src/M5StickCPlus/blynk-to-buruburu/blynk-to-buruburu.ino
+++ src/M5StickCPlus/blynk-to-buruburu/blynk-to-buruburu.ino
@@ -2,6 +2,7 @@
 
 #include <WiFi.h>
 #include "my-blynk-info.h"
+#include "image.h"
 
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
@@ -19,12 +20,15 @@ void setup()
   Blynk.begin(auth, ssid, pass);
 
   M5.Axp.ScreenBreath(10);
-  M5.Lcd.setRotation(3);
+  M5.Lcd.setRotation(0);
+  /*
   M5.Lcd.fillScreen(WHITE);
   M5.Lcd.setCursor(5, 10);
   M5.Lcd.setTextColor(BLACK);
   M5.Lcd.setTextSize(2);
   M5.Lcd.printf("iBou by Komorebi");
+  */
+  M5.Lcd.pushImage(0, 0, IMG_WIDTH, IMG_HEIGHT, IMG_DATA[1]);
 
   pinMode(33, OUTPUT);
 }
@@ -37,15 +41,19 @@ void loop()
 
 BLYNK_WRITE(V1)
 {
-  Serial.printf("Button V1 = %d\r\n", param[0]);
+  Serial.printf("Button V1 = %d\r\n", param[0].asStr());
 
+  /*
   M5.Lcd.setCursor(0, 120);
   M5.Lcd.fillRect(0, 120, 320, 20, BLACK);
   M5.Lcd.printf("V1:%d", param[0]);
+  */
 
   if ( param[0] == 1 ) {
     digitalWrite(33, HIGH);
+    M5.Lcd.pushImage(0, 0, IMG_WIDTH, IMG_HEIGHT, IMG_DATA[1]);
   } else {
     digitalWrite(33, LOW);
+    M5.Lcd.pushImage(0, 0, IMG_WIDTH, IMG_HEIGHT, IMG_DATA[0]);
   }
 }
